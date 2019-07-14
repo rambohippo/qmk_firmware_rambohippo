e245fa97c917fa455d3394640d4f6ceb31746641
Conrad <jcmcgarry6@gmail.com>
2019-07-14 13:32:35 -0700

    [keymap] Minor updates to Adjust layer and formatting
    - Minor formatting updates
    - Remove alt-tab and shift-alt-tab from adjust layer

--- keyboards/keebio/nyquist/keymaps/rambohippo/keymap.c
+++ keyboards/keebio/nyquist/keymaps/rambohippo/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] =  LAYOUT( \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  ALT_TAB,  _______, KC_UP, _______,  KC_PGUP, _______, _______, _______, _______, _______, _______, _______, \
+  ALT_TAB, _______, KC_UP,   _______, KC_PGUP, _______, _______, _______, _______, _______, _______, _______, \
   _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, ALT_TAB, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
   KC_LSFT, _______, _______, _______, KC_ENT,  KC_BSPC, _______, _______, KC_HOME, KC_END,  _______, _______, \
   KC_F5,   _______, _______, _______, _______, _______, KC_SPC,  _______, _______, _______, _______, _______ \
@@ -94,8 +94,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, \
   _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD, KC_GRV,  KC_MINS, KC_BSLS, _______, _______, \
   KC_CAPS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_ASTR, _______, KC_PLUS, KC_EQL,  KC_UNDS, _______, KC_VOLU, \
-  KC_LSFT, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_AMPR, _______, KC_QUOT, KC_DQUO, _______,  _______, KC_VOLD, \
-  _______, _______, _______, _______, _______,  KC_SPC, _______, _______, _______, _______, _______, _______ \
+  KC_LSFT, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_AMPR, _______, KC_QUOT, KC_DQUO, _______, _______, KC_VOLD, \
+  _______, _______, _______, _______, _______, KC_SPC,  _______, _______, _______, _______, _______, _______ \
 ),
 
 /* NUMPAD - Arrows are not in a good location. Consider reworking left side
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
  * |      | STab |  Up  |  Tab |      |      |  |      |   7  |   8  |   9  |   -  | Bksp |
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
- * |      | Right| Down | Left |      |      |  |      |   4  |   5  |   6  |   +  |      |
+ * |      | Left | Down | Right|      |      |  |      |   4  |   5  |   6  |   +  |      |
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
  * | Shift|      |      |      |      |      |  |      |   1  |   2  |   3  |      |      |
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_NUMPAD] =  LAYOUT( \
   _______, _______,   _______,  _______, _______, _______, _______, _______, KC_PSLS, KC_PAST, KC_PEQL, KC_DEL, \
   _______, S(KC_TAB), KC_UP,    KC_TAB,  _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_BSPC, \
-  _______, KC_RGHT,   KC_DOWN,  KC_LEFT, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______, \
+  _______, KC_LEFT,   KC_DOWN,  KC_RGHT, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______, \
   KC_LSFT, _______,   _______,  _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, _______, \
   _______, _______,   _______,  _______, _______, _______, KC_SPC,  KC_P0,   KC_COMM, KC_PDOT, KC_PENT, _______ \
 ),
@@ -134,8 +134,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] =  LAYOUT( \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  _______, _______, _______, CTAB_BK, CTAB_FW, WN_DKCL, _______, _______, _______, _______, _______, KC_F12, \
-  KC_DEL,  _______, _______, ATAB_BK, ATAB_FW, WN_DKNW, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, KC_PSCR, \
+  _______, _______, _______, _______, _______, WN_DKCL, _______, _______, _______, _______, _______, KC_F12, \
+  KC_DEL,  _______, _______, CTAB_BK, CTAB_FW, WN_DKNW, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, KC_PSCR, \
   KC_LSFT, LN_DKBK, LN_DKFW, WN_DKBK, WN_DKFW, _______,   _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET \
 )
