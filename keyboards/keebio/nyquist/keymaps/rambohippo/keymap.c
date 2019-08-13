df434e952920994102707cc9493236f5dfccf5b1
Rambohippo <jcmcgarry6@gmail.com>
2019-08-12 19:19:27 -0700

    [Keymap] Move keys in lower layers off the top row
    - Remove alt-tab keycodes
    - Replace enter in bottom right corner with esc
    - Lower - remove arrow keys
    - Lower - replace alt-tab with gui-tab
    - lower - add numbers to second row
    - adjust - remove delete

--- keyboards/keebio/nyquist/keymaps/rambohippo/keymap.c
+++ keyboards/keebio/nyquist/keymaps/rambohippo/keymap.c
@@ -28,8 +28,7 @@ enum custom_keycodes {
 #define WN_DKBK LCTL(LGUI(KC_LEFT))     // Windows - Previous Desktop
 #define WN_DKNW LCTL(LGUI(KC_D))        // Windows - New Desktop
 #define WN_DKCL LCTL(LGUI(KC_F4))       // Windows - Close Desktop
-#define ATAB_FW LALT(KC_TAB)            // Alt-Tab
-#define ATAB_BK LSFT(LALT(KC_TAB))      // Shift-Alt-Tab
+#define GUI_TAB LGUI(KC_TAB)            // Gui-Tab
 #define CTAB_FW LCTL(KC_TAB)            // Ctrl-Tab
 #define CTAB_BK LSFT(LCTL(KC_TAB))      // Shift-Ctrl-Tab
 
@@ -45,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |  |   N  |   M  |   ,  |   .  |   /  | Shift|
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
- * | Gui  | Ctrl | Alt  |Numpad| Lower| Space|  | Enter| Raise| Ctrl | Left |  Up  | Enter|
+ * | Gui  | Ctrl | Alt  |Numpad| Lower| Space|  | Enter| Raise| Ctrl | Left | Right| Esc  |
  * `-----------------------------------------'  `-----------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
@@ -53,28 +52,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
-  KC_LGUI, KC_LCTL, KC_LALT, NUMPAD,  LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_RCTL, KC_LEFT, KC_RGHT,   KC_ENT \
+  KC_LGUI, KC_LCTL, KC_LALT, NUMPAD,  LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_RCTL, KC_LEFT, KC_RGHT, KC_ESC \
 ),
 
 /* Lower
  * ,-----------------------------------------.  ,-----------------------------------------.
  * |      |      |      |      |      |      |  |      |      |      |      |      |      |
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
- * |AltTab|      |  Up  |      |PageUp|      |  |      |      |      |      |      |      |
+ * |GuiTab|   1  |   2  |   3  |   4  |   5  |  |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
- * |      | Left | Down | Right|PageDn|AltTab|  | Left | Down |  Up  | Right|      |      |
+ * |      |      |      |PageUp|PageDn|AltTab|  | Left | Down |  Up  | Right|      |      |
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
  * | Shift|      |      |      | Enter| Bksp |  |      |      | Home | End  |      |      |
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
- * |  F5  |      |      |      | ---- |      |  | Space|      |      |      |      |      |
+ * |  F5  |      |      |      | ---- |      |  |      |      |      |      |      |      |
  * `-----------------------------------------'  `-----------------------------------------'
  */
 [_LOWER] =  LAYOUT( \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  ALT_TAB, _______, KC_UP,   _______, KC_PGUP, _______, _______, _______, _______, _______, _______, _______, \
-  _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, ALT_TAB, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
+  GUI_TAB, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
+  _______, _______, _______, KC_PGUP, KC_PGDN, ALT_TAB, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
   KC_LSFT, _______, _______, _______, KC_ENT,  KC_BSPC, _______, _______, KC_HOME, KC_END,  _______, _______, \
-  KC_F5,   _______, _______, _______, _______, _______, KC_SPC,  _______, _______, _______, _______, _______ \
+  KC_F5,   _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______ \
 ),
 
 /* RAISE
@@ -108,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
  * | Shift|      |      |      |      |      |  |      |   1  |   2  |   3  |      |      |
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
- * | ---- |      |      |      |      |      |  | Space|   0  |   ,  |   .  | Enter|      |
+ * |      |      |      | ---- |      |      |  | Space|   0  |   ,  |   .  | Enter|      |
  * `-----------------------------------------'  `-----------------------------------------'
  */
 [_NUMPAD] =  LAYOUT( \
@@ -123,21 +122,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.  ,-----------------------------------------.
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
- * |      |      |      |SC Tab| C Tab|WinDCL|  |      |      |      |      |      |  F12 |
+ * |      |      |      |      |      |      |  |      |      |      |      |      |  F12 |
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
- * | Del  |      |      |SA Tab| A TAb|WinDNW|  | Mute | Vol -| Vol +| Play |      |PrtScn|
+ * |      |      |      |SC Tab| C TAb|WinDNW|  | Mute | Vol -| Vol +| Play |      |      |
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
- * | Shift|LinPre|LinNxt|WinPre|WinNxt|      |  |      |      |      |      |      |      |
+ * | Shift|LinPre|LinNxt|WinPre|WinNxt|WinDCL|  |      |      |      |      |      |      |
  * |------+------+------+------+------+------|  |------+------+------+------+------+------|
- * |      |      |      |      | ---- |      |  |      | ---- |      |      |      | Reset|
+ * |      |      |      |      | ---- |      |  |      | ---- |      |      |      |PrtScn|
  * `-----------------------------------------'  `-----------------------------------------'
  */
 [_ADJUST] =  LAYOUT( \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  _______, _______, _______, _______, _______, WN_DKCL, _______, _______, _______, _______, _______, KC_F12, \
-  KC_DEL,  _______, _______, CTAB_BK, CTAB_FW, WN_DKNW, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, KC_PSCR, \
-  KC_LSFT, LN_DKBK, LN_DKFW, WN_DKBK, WN_DKFW, _______,   _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F12, \
+  _______, _______, _______, CTAB_BK, CTAB_FW, WN_DKNW, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, _______, \
+  KC_LSFT, LN_DKBK, LN_DKFW, WN_DKBK, WN_DKFW, WN_DKCL, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR \
 )
 
 /* BLANK
